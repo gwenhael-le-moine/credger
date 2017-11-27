b27922ed533d80d99875ee1d4696710788b6f8b6
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-11-27 15:19:10 +0100

    caching troubles

--- public/ts/services/API.ts
+++ public/ts/services/API.ts
@@ -3,7 +3,7 @@ app.service('API',
     function($http) {
       let API = this;
 
-      API.balance = _.memoize(function(period, categories, depth) {
+      API.balance = function(period, categories, depth) {
         return $http.get('/api/ledger/balance', {
           params: {
             period: period,
@@ -11,25 +11,25 @@ app.service('API',
             depth: depth
           }
         });
-      });
+      };
 
-      API.register = _.memoize(function(period, categories) {
+      API.register = function(period, categories) {
         return $http.get('/api/ledger/register', {
           params: {
             period: period,
             categories: categories
           }
         });
-      });
+      };
 
-      API.graph_values = _.memoize(function(period, categories) {
+      API.graph_values = function(period, categories) {
         return $http.get('/api/ledger/graph_values', {
           params: {
             period: period,
             categories: categories
           }
         });
-      });
+      };
 
       API.accounts = _.memoize(function() {
         return $http.get('/api/ledger/accounts');
